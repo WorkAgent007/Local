033940c949e28a903c43c94d9aef896d5371246a
Dima Patyk <patyk06@mail.ru>
2024-09-24 08:43:46 +0300

    First task

--- main.c
+++ main.c
@@ -2,7 +2,12 @@
 
 
 int main(void) {
-    printf("Hello world!\n");
+    int a, b;
+    printf("Введите первое число");
+    scanf("%d", &a);
+    printf("Введите второе число");
+    scanf("%d", &b);
+
 
     return 0;
 }
